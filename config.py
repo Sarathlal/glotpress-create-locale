e549963011beaa162f59d3c85a4071421041edd9
Sarathlal <hello@sarathlal.com>
2023-05-30 18:21:48 +0530

    compleeted

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
-import os 
+import os
 from dotenv import load_dotenv
 load_dotenv()
 
+# # Set the working directory
+# os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 # Find the absolute file path to the top level project directory
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -10,10 +12,19 @@ class Config:
     """
     Base configuration class. Contains default configuration settings + configuration settings applicable to all environments.
     """
-    # Default settings
-    SITE_URL = os.getenv('SITE_URL', default='')
-    ADMIN_URL = os.getenv('SITE_URL') + '/wp-login.php'
+    #GlotPress params
+    GLOTPRESS_URL_BASE = 'portal'
+    GLOTPRESS_PROJECT_ID = 1
 
+    # URLs
+    SITE_URL = os.getenv('SITE_URL', default='')
+    LOGIN_URL = os.getenv('SITE_URL') + '/wp-login.php'
+    GLOTPRESS_URL = SITE_URL + GLOTPRESS_URL_BASE
+
+
+    # Credentails
     USER_NAME = os.getenv('USER_NAME')
     PASSWORD = os.getenv('PASSWORD')
-    # DEBUG = False
+
+    # WP locale CSV file
+    LOCALE_LIST = basedir + '/wp-locale.csv'
--- main.py
+++ main.py
@@ -1,27 +1,20 @@
+import csv
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.ui import WebDriverWait 
+from selenium.webdriver.support import expected_conditions as EC
 
 from config import Config
  
 driver = webdriver.Firefox()
 
-# #launch URL
-# url = Config.SITE_URL
-# driver.get(url)
-
-# #identify search box
-# m = driver.find_element(by=By.NAME, value="q")
-
-# #enter search text
-# m.send_keys("Open Source")
-
-# #perform Google search with Keys.ENTER
-# m.send_keys(Keys.ENTER)
-
+# Login to WordPress
 def login():
-    url = Config.ADMIN_URL
-    driver.get(url)
+    driver.get(Config.LOGIN_URL)
 
     # Target the user name field
     user_name_field = driver.find_element(by=By.ID, value="user_login")
@@ -37,6 +30,51 @@ def login():
 
     driver.find_element(by=By.ID, value="loginform").submit()
 
+def visit_frontend():
+    driver.get(Config.SITE_URL)
+
+# Iterate throgh locale file
+def iterate_locale():
+    # Open file 
+    with open(Config.LOCALE_LIST) as file_obj:
+        
+        # Create reader object by passing the file object
+        reader_obj = csv.reader(file_obj)
+        
+        # Iterate over each row in the csv file
+        for row in reader_obj:
+            time.sleep(2)
+            create_locale(row[0], row[1])
+
+# Create locale
+def create_locale(locale_code, locale_name):
+
+    create_locale_url = Config.GLOTPRESS_URL + '/sets/-new/?project_id=' + str(Config.GLOTPRESS_PROJECT_ID)
+    driver.get(create_locale_url)
+ 
+    # Scroll to top
+    driver.execute_script("window.scrollTo(0, 0)")
+
+    time.sleep(1)
+
+    sel = Select(WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.ID, "set[locale]"))
+    ))
+    sel.select_by_value(locale_code)
+
+    time.sleep(1)
+
+    # Target the locale name field
+    locale_name_field = driver.find_element(by=By.ID, value="set[name]")
+    # Fill the locale name
+    locale_name_field.send_keys(locale_name)
+
+    time.sleep(1)
+
+    # Target the submit button
+    form = driver.find_element(by=By.CLASS_NAME, value="gp-content").find_element(by=By.TAG_NAME, value="form").submit()
 
 
 login()
+time.sleep(1)
+iterate_locale()
